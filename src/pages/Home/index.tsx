8a97ba3b92099e5ee870e58ff0701ca76073ada4
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2023-03-20 21:27:55 -0300

    Ajuste CSS

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,8 @@
 import { Container } from './styles'
+import { Select } from '@/components/Select'
+
+import dogHomeHeader from '../../assets/images/dog_home.svg'
+import dogsHome from '../../assets/images/dogs_home.png'
 
 export function Home() {
   function handleSearchPets() {
@@ -13,5 +17,25 @@ export function Home() {
     // TO DO
   }
 
-  return <Container>{/* COMECE POR AQUI */}</Container>
+  return (
+    <Container>
+      <div>
+        <img src={dogHomeHeader} alt="Desenho de um rosto de cachorro" />
+        FindAFriend
+      </div>
+      <div>Leve a felicidade para o seu Lar</div>
+      <div>
+        <img src={dogsHome} alt="Imagens de 6 carros sorrindo" />
+      </div>
+      <div>Encontre o animal de estimação ideal para seu estilo de vida!</div>
+      <div>
+        Busque um amigo:
+        <Select label="" name="" options={[]} />
+        <Select label="" name="" options={[]} />
+        <button>
+          <img src="" alt="" />
+        </button>
+      </div>
+    </Container>
+  )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -2,7 +2,9 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
   display: flex;
+  align-items: center;
   justify-content: center;
+  flex-direction: column;
   min-height: 100vh;
   background-color: ${(props) => props.theme.primary};
 `
